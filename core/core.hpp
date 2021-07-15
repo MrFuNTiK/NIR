78371e6dde07b78e38087e1b1e96af6865bc7360
MrFuNTiK <voevodin111197@gmail.com>
2021-07-16 00:30:07 +0700

    Fix mistake in discription of some functions.

--- core/core.hpp
+++ core/core.hpp
@@ -18,7 +18,7 @@ uint8_t swap(T* first, T*second)
     return 0;
 }
 
-/// @brief Function calculates GPS spectrum of given fourier image of signal and writes it to given pointer.
+/// @brief Function calculates phase spectrum of given fourier image of signal and writes it to given pointer.
 /// Memory for spectrum should be allocated before call of this function.
 ///
 /// @param size             - amount of complex elements in given fourier image
@@ -26,10 +26,10 @@ uint8_t swap(T* first, T*second)
 /// @param spectrum         - pointer where GPS spectrum will be written
 void get_phase_spectrum(uint16_t size, fftw_complex* fourier_image, double* spectrum);
 
-/// @brief Function unwraps GPS spectrum
+/// @brief Function unwraps phase spectrum
 ///
 /// @param size             - amount of samples in given spectrum
-/// @param phase_spectrum   - pointer to GPS spectrum to unwrap
+/// @param phase_spectrum   - pointer to phase spectrum to unwrap
 void unwrap_phase_spectrum(uint16_t size, double* phase_spectrum);
 
 #endif // CORE_H

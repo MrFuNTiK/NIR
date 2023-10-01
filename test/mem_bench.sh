2c30ab1587a235c53976941e3a5183098d9bf22d
MrFuNTiK <voevodin111197@gmail.com>
2023-10-01 12:15:15 +0700

    Some fixes.

--- test/mem_bench.sh
+++ test/mem_bench.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Run command: ./mem_bench.sh > ../_result/bin/mem_res.txt 2>&1
+
 BINARY_DIR=${CWD}../_result/bin
 TEST_NAME=tests
+RES_FILE=${BINARY_DIR}/mem_res_44100.txt
 
-win_sizes=(128 256 512 1024 2048 4096 8192)
+win_sizes=(256 512 1024 2048 4096 8192)
 tde_meths=(GCC GPS GPS_GRZ)
 
 for size in ${win_sizes[*]}
